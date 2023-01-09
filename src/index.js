5300b9053becb5d530620b3cc71e7e268207cfaf
mariaruizgomez <crg.7886@gmail.com>
2023-01-09 15:12:21 -0800

    Added function to display the current date and time of a location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  let lasVegasElement = document.querySelector("#las-vegas");
+  let lasVegasDateElement = lasVegasElement.querySelector(".date");
+  let lasVegasTimeElement = lasVegasElement.querySelector(".time");
+  let lasVegasTime = moment().tz("America/Los_Angeles");
+  lasVegasDateElement.innerHTML = lasVegasTime.format("MMMM Do YYYY");
+  lasVegasTimeElement.innerHTML = lasVegasTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  let MadridElement = document.querySelector("#madrid");
+  let MadridDateElement = MadridElement.querySelector(".date");
+  let MadridTimeElement = MadridElement.querySelector(".time");
+  let MadridTime = moment().tz("Europe/Madrid");
+  MadridDateElement.innerHTML = MadridTime.format("MMMM Do YYYY");
+  MadridTimeElement.innerHTML = MadridTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
